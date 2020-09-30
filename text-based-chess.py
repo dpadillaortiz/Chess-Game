ef0dad887b00b587b7f9a6f45fdcc4d158c22a9f
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2020-09-30 14:13:43 -0700

    description of changes in documentation text file

--- text-based-chess.py
+++ text-based-chess.py
@@ -1,51 +1,58 @@
 #Classes and Definitions
 
-class Pawn:
+
+class ChessPiece:
 	def __init__(self, x, y):
 		self.xaxis = x
 		self.yaxis = y
-	
 	def cord(self):
 		cord = [self.xaxis, self.yaxis]
 		print(cord)
+	def onBoard(self, x, y):
+		xOnBoard = x in ['A','B','C','D','E','F','G','H'] 	
+		if y not in [1,2,3,4,5,6,7,8]:
+			print("You're off the board")
+		
 
+class Pawn(ChessPiece):
+	if 
 	def movePawn(self, x, y):
-		if x != self.xaxis:
+		if (x != self.xaxis) or (y > 8):
 			print("Can't move there")
 		else: 
 			self.yaxis = y		 
-		
-# Testing Zone
+			self.cord()
 
+class Rook(ChessPiece):
+	def moveRook(self, x, y):
+		# moving up/down a col
+		if x == self.xaxis:
+			self.yaxis = y
+			self.cord()	
+		# moving left/right a row 
+		elif y == self.yaxis:
+			self.xaxis = x
+			self.cord()
+		else:
+			print("Can't move there")
+	
+#Testing Zone
+'''
 pawnTest = Pawn('A', 2)
 print(pawnTest.xaxis, pawnTest.yaxis)
 pawnTest.cord()
 pawnTest.movePawn('A',10)
 pawnTest.movePawn('B',2)
 pawnTest.cord()
+'''
 
+rTest = Rook('E', 1)
+rTest.cord()
+rTest.moveRook('E', 2)
+rTest.moveRook('D', 2)
+rTest.moveRook('D', 6)
+rTest.moveRook('E', 6)
+rTest.moveRook('A', 1)
+rTest.moveRook('A', 6)
 
-
-"""
-pawn = ['a',1]
-
-print(pawn)
-def updatePos(piece, x, y):
-	if (x != pawn[0]):
-		print("Pawn does not move there")
-	if ((turn != 0) and (y == y + 2)):
-		print("Pawn can only move up one")
-	else: 
-		piece[0] = x
-		piece[1] = y
-	
-
-updatePos(pawn, 'b', 3)
-print(pawn)
-turn = 4
-print("turn = ", turn)
-updatePos(pawn, 'a', 4)
-print(pawn)
-
-"""
 		 
