18723a04247214ba286228b01c1e60eddf969c8a
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2020-09-30 22:50:03 -0700

    I'm trying to figure out why after moveRook sets it to 2,6 the other cords arn't set

--- text-based-chess.py
+++ text-based-chess.py
@@ -4,15 +4,27 @@ nums = [1,2,3,4,5,6,7,8]
 
 class ChessPiece:
 	def __init__(self, x, y):
-		if (x in letters and y in nums):
-			self.xaxis = x
-			self.yaxis = y
-		else:
-			print("The piece is off the board")
+		self.xaxis = x
+		self.yaxis = y
 	def cord(self):
 		cord = [self.xaxis, self.yaxis]
 		print(cord)
-			
+	def move(self, x, y):
+		# moving up/down a col
+		if x == self.xaxis:
+			self.yaxis = y
+			self.cord()	
+		# moving left/right a row 
+		elif y == self.yaxis:
+			self.xaxis = x
+			self.cord()	
+	def onBoard(self):
+		if self.xaxis not in letters:
+			print("not in letters")
+			return False
+		if self.yaxis not in nums:
+			print("not in numbers")
+			return False
 
 class Pawn(ChessPiece): 
 	def movePawn(self, x, y):
@@ -22,35 +34,46 @@ class Pawn(ChessPiece):
 			self.yaxis = y		 
 			self.cord()
 
-class Rook(ChessPiece):
+class Rook(ChessPiece):	
 	def moveRook(self, x, y):
-		# moving up/down a col
-		if x == self.xaxis:
-			self.yaxis = y
-			self.cord()	
-		# moving left/right a row 
-		elif y == self.yaxis:
-			self.xaxis = x
-			self.cord()
+		if self.onBoard() == False:
+			print("Rook is off the board")
+			print([self.xaxis,self.yaxis])
 		else:
-			print("The rook can't move there")
-	
-#Testing Zone
+			self.move(x, y) 
 
+
+
+
+#--------------------------------------
+#Testing Zone
+'''
 pawnTest = Pawn('A', 2)
 pawnTest.cord()
 pawnTest.movePawn('A',10)
 pawnTest.movePawn('B',2)
 pawnTest.cord()
-
+'''
 
 rTest = Rook('E', 1)
 rTest.cord()
 rTest.moveRook('E', 2)
 rTest.moveRook('D', 2)
 rTest.moveRook('D', 6)
+rTest.moveRook('2', 6)
+rTest.moveRook('X', 6)
+rTest.moveRook('D', 11)
 rTest.moveRook('E', 6)
 rTest.moveRook('A', 1)
 rTest.moveRook('A', 6)
 
-		 
+'''
+qTest = Queen('A', 4)
+qTest.cord()
+qTest.moveRook('E', 2)
+qTest.moveRook('D', 2)
+qTest.moveRook('D', 6)
+qTest.moveRook('E', 20)
+qTest.moveRook('A', 1)
+qTest.moveRook('A', 6)
+'''
