3b9e95f50cb539ff3e6cb8bcf2a2ca517ceb5b6a
Daniel <padillaortiz.daniel099@gmail.com>
2022-02-02 21:54:06 -0800

    new chess file update

---
+++ chess.py
@@ -0,0 +1,40 @@
+class Chessboard:
+    def __init__(self, piece):
+        self.board = [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]]
+        self.piece = piece
+
+        if "A" in self.piece.position:
+            self.board[0][2] = self.piece
+    def __str__(self):
+        return self.board
+
+class Chesspiece:
+    def __init__(self, name, position, board):
+        self.name = name
+        self.position = position
+        self.board = board
+    def __repr__(self):
+        return self.name + " is on " + self.position
+    def move_piece(self, new_pos):
+        self.position = new_pos
+        if "F" in self.position:
+            if self.board[0][2] == None:
+                self.board[0][2] = self.position
+                print(self.name,"moved to",self.board[0][2])
+
+
+board = [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]]
+
+rook = Chesspiece("rook","A2",board)
+print(rook)
+
+#chessb = Chessboard(rook)
+#print(chessb)
+
+#test = [None,None,None]
+#print(test)
+
+rook.move_piece("F4")
+#print(rook)
+
+print(board)
