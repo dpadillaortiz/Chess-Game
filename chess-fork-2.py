8d6b30ebd8c7a10177b0964c2b570c2e12f890f4
Daniel <padillaortiz.daniel099@gmail.com>
2022-03-03 20:56:30 -0800

    working on chesspiece class

--- chess-fork-2.py
+++ chess-fork-2.py
@@ -1,6 +1,8 @@
 # Chess Project
 
+from curses import setupterm
 import string
+from sys import settrace
 
 class Chessboard:
     def __init__(self):
@@ -14,15 +16,15 @@ class Chessboard:
         self.__board_key = {key:value for key, value in zip(self.__board_axis, [x for x in range(8)] + [x for x in range(8)])}
         # Creates list ["A1", ..., "A8", ..., "H1", ..., "H8"]
         self.__board_positions = [self.__letters[y] + self.__numbers[x] for y in range(len(self.__letters)) for x in range(len(self.__numbers))]
-    def __repr__(self):
-        return str(self.__board)
 
     # Checks if position is available
     def spot_available(self, position):
-        self.x_pos = self.__board_key[position[0]]
-        self.y_pos = self.__board_key[position[1]]
-        if self.__board[self.x_pos][self.y_pos] == None:
-            return True
+        self.x_axis = self.__board_key[position[0]]
+        self.y_axis = self.__board_key[position[1]]
+        position = position.upper()
+        if position in self.__board_positions:
+            if self.__board[self.x_axis][self.y_axis] == None:
+                return True
 
     # Prints board in a legible way 
     def print_board(self):
@@ -35,23 +37,54 @@ class Chessboard:
     def get_board(self):
         return self.__board
     
+    # defining setter for board
     @get_board.setter
     def update_board(self, position):
         # using __board_positions to check valid position 
         # is replacing the need for on_board
-        position = position.upper()
-        if position in self.__board_positions:
-            if self.spot_available(position):
-                self.__board[self.x_pos][self.y_pos] = position
+        if self.spot_available(position) == True:
+            self.__board[self.x_axis][self.y_axis] = position
         else:
             print("Position is unavailable")
-    
+
+
+class Chesspiece:
+    def __init__(self, name, position, chessboard = None):
+        self.__name = name
+        self.__position = position
+        self.__chessboard = chessboard
+        self.__chessboard.update_board = self.__position
+    # defining getter for chesspiece
+    @property
+    def get_position(self):
+        return self.__position
+    # defining setter
+    @get_position.setter
+    def set_position(self, position):
+        print("no cond")
+        #self.to_pos = position
+        #self.from_pos = self.__position
+        if self.__chessboard.spot_available(position) == True:
+            print("in cond")
+            self.__position = position
+            self.__chessboard.update_board = position
+        else:
+             print("wtf")
+
+
+
+
 
 
 
 test = Chessboard()
+test.print_board()
+print()
 test.update_board = "A3"
 test.print_board()
-test.update_board = "r4"
-test.update_board = "h3"
+print()
+rook = Chesspiece("Rook", "B3", test)
+test.print_board()
+print()
+rook.set_position = "C3"
 test.print_board()
