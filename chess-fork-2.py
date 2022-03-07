9e67e32a55ce2618fe41b9990e45392c303f26ea
Daniel <padillaortiz.daniel099@gmail.com>
2022-03-06 21:53:35 -0800

    Fixed the calc_dist
    could not call __board_key because it was private so I switched it to _board_key
    My understanding was that python encapsulation wasn't a thing because all variables were public - the protected and private variable syntax was just notation

--- chess-fork-2.py
+++ chess-fork-2.py
@@ -11,14 +11,14 @@ class Chessboard:
         # Creates list ['A', ..., 'H','1', ..., '8']
         self.__board_axis = self.__letters + self.__numbers
         # Creates {'A':0, ..., 'H':7, '1':0, ..., '8':7}
-        self.__board_key = {key:value for key, value in zip(self.__board_axis, [x for x in range(8)] + [x for x in range(8)])}
+        self._board_key = {key:value for key, value in zip(self.__board_axis, [x for x in range(8)] + [x for x in range(8)])}
         # Creates list ["A1", ..., "A8", ..., "H1", ..., "H8"]
         self.__board_positions = [self.__letters[y] + self.__numbers[x] for y in range(len(self.__letters)) for x in range(len(self.__numbers))]
 
     # Checks if position is available
     def spot_available(self, position):
-        self.x_axis = self.__board_key[position[0]]
-        self.y_axis = self.__board_key[position[1]]
+        self.x_axis = self._board_key[position[0].upper()]
+        self.y_axis = self._board_key[position[1].upper()]
         position = position.upper()
         if position in self.__board_positions:
             if self.__board[self.x_axis][self.y_axis] == None:
@@ -50,30 +50,53 @@ class Chesspiece:
         self.__position = position
         self.__chessboard = chessboard
         self.__chessboard.update_board = (self.__name, self.__position)
+    
+    # defining name getter
+    @property
+    def get_name(self):
+        return self.__name
+    
     # defining getter for chesspiece
     @property
     def get_position(self):
         return self.__position
+
     # defining setter
     @get_position.setter
     def set_position(self, position):
         self.to_pos = position
         self.from_pos = self.__position
         if self.__chessboard.spot_available(self.to_pos) == True:
+            # update position and update the board
             self.__position, self.__chessboard.update_board = self.to_pos, (self.__name, self.to_pos)
             print("{} moved from {} to {}".format(self.__name, self.from_pos, self.to_pos))
 
+    def calc_dist(self, position):
+        self.to_pos = position.upper()
+        self.from_pos = self.__position
+        self.a = self.__chessboard._board_key[self.to_pos[1]] - self.__chessboard._board_key[self.from_pos[1]]
+        self.b = self.__chessboard._board_key[self.to_pos[0]] - self.__chessboard._board_key[self.from_pos[0]]
+        return (self.a, self.b)
 
+
+class Rook(Chesspiece):
+    def __init__(self, position, chessboard = None):
+        super().__init__("Rook", position, chessboard)
+    def move_to(self, position):
+        a_side, b_side = self.calc_dist(position)
+        if a_side**2 == 0:
+            self.set_position = position
+        elif b_side**2 == 0:
+            self.set_position = position
 
 
 
 
 
 test = Chessboard()
-rook = Chesspiece("Rook", "B3", test)
-test.print_board()
-print()
-rook.set_position = "C3"
+rook = Rook('C3', test)
+rook.move_to("C6")
+rook.move_to("e6")
 test.print_board()
 
 
