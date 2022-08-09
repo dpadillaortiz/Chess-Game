9013a7e0b86d081aea7ed42cf6ab51dc4fbb4a2d
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2022-08-08 22:03:08 -0700

    Modified movement_tests.py
    Using valid_moves, i'm trying to create a parameterized test for each chesspiece using (4,4) as the initial postion like in the main code in valid_moves.py

--- movement_tests.py
+++ movement_tests.py
@@ -2,6 +2,7 @@
 
 import movement as move
 import unittest
+import valid_moves as valid_move
 
 class TestDistance(unittest.TestCase):
     def test_distance(self):
@@ -9,6 +10,22 @@ class TestDistance(unittest.TestCase):
 
 class TestMovement(unittest.TestCase):
     def test_queen_to(self):
-        self.assertEqual(move.queen_to((2,1),(4,1)), (4,1), 'Expected (4,1)')
+        initial_position = (4,4)
+        coordinate_list = valid_move.generate_coordinates()
+        valid_queen_moves_list = valid_move.valid_queen_moves(initial_position, coordinate_list)
+        valid_queen_move = valid_queen_moves_list[4]
+        move_queen_to = move.queen_to(initial_position, valid_queen_move)
+        self.assertEqual(move_queen_to, valid_queen_move, f'Expected {valid_queen_move}')
+
+    def test_king_to(self):
+        initial_position = (4,4)
+        coordinate_list = valid_move.generate_coordinates()
+        valid_king_move_list = valid_move.valid_king_moves(initial_position, coordinate_list)
+        for index, coordinate in enumerate(coordinate_list):
+            print(index)
+            with self.subTest(coordinate):
+                valid_king_move = valid_king_move_list[index]
+                move_king_to = move.king_to(initial_position, valid_king_move)
+                self.assertEqual(move_king_to, valid_king_move, f'Expected {valid_king_move}')
 
 unittest.main()
