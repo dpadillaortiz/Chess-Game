f444a91b11a3d94b6df84c94d31462affde6f5f6
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2022-08-08 21:59:57 -0700

    Complete valid tests
    There's a valid move function for each piece.
    
    Takes an initial position (tuple) and a list of coordinates (list of tuples) then returns a list of all from moves from the inital position to each coordinate in the coordinate list

--- valid_moves.py
+++ valid_moves.py
@@ -27,13 +27,57 @@ def valid_king_moves(initial_position: tuple, coordinate_list: list) -> list:
             valid_moves.append(valid_move)
     return valid_moves
 
+def valid_bishop_moves(initial_position: tuple, coordinate_list: list) -> list:
+    valid_moves = []
+    for coordinate in coordinate_list:
+        valid_move = move.bishop_to(initial_position, coordinate)
+        if valid_move == initial_position:
+            continue
+        if valid_move != None:
+            valid_moves.append(valid_move)
+    return valid_moves
 
+def valid_knight_moves(initial_position: tuple, coordinate_list: list) -> list:
+    valid_moves = []
+    for coordinate in coordinate_list:
+        valid_move = move.knight_to(initial_position, coordinate)
+        if valid_move == initial_position:
+            continue
+        if valid_move != None:
+            valid_moves.append(valid_move)
+    return valid_moves
+
+
+def valid_rook_moves(initial_position: tuple, coordinate_list: list) -> list:
+    valid_moves = []
+    for coordinate in coordinate_list:
+        valid_move = move.rook_to(initial_position, coordinate)
+        if valid_move == initial_position:
+            continue
+        if valid_move != None:
+            valid_moves.append(valid_move)
+    return valid_moves
+
+
+def valid_pawn_moves(initial_position: tuple, coordinate_list: list) -> list:
+    valid_moves = []
+    for coordinate in coordinate_list:
+        valid_move = move.pawn_to(initial_position, coordinate)
+        if valid_move == initial_position:
+            continue
+        if valid_move != None:
+            valid_moves.append(valid_move)
+    return valid_moves
 
 if __name__ == '__main__':
     coordinates = generate_coordinates()
     initial_position = (4,4)
     valid_moves_for = {'queen': valid_queen_moves(initial_position, coordinates),
-            'king': valid_king_moves(initial_position, coordinates)
+            'king': valid_king_moves(initial_position, coordinates),
+            'bishop': valid_bishop_moves(initial_position, coordinates),
+            'knight': valid_knight_moves(initial_position, coordinates),
+            'rook': valid_rook_moves(initial_position, coordinates),
+            'pawn': valid_pawn_moves(initial_position, coordinates)
     }
     for piece in valid_moves_for:
         print(f'Valid moves for {piece}:\n'+str(valid_moves_for[piece])+'\n')
