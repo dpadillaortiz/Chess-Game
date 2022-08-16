d8af3acb2fec6a713038d130bc1801e48983d62a
dpadillaortiz <padillaortiz.daniel099@gmail.com>
2022-08-15 22:20:05 -0700

    Factored the code to use the logic from chess_tools

--- movement.py
+++ movement.py
@@ -1,63 +1,34 @@
 #! /bin/usr/python3
 
-def distance_between(initial_position: tuple, final_position: tuple) -> tuple:
-    '''Distance between two points 
-    i.e. Pythagoras theorem
-    d^2 = (x_2 - x_1)^2 + (y_2 - y_1)^2
-    where (x_2 - x_1) and (y_2 - y_1) are not the hypothenuse 
-    Note: returns (x_2 - x_1, y_2 - y_1)
-    '''
-    initial_x, initial_y = initial_position
-    final_x, final_y = final_position
-    x = final_x - initial_x
-    y = final_y - initial_y
-    return (x, y)
+import chess_tools
 
 def pawn_to(from_position: tuple, to_position: tuple) -> tuple:
-    run, rise = distance_between(from_position, to_position)
-    moved_vertically_once = run**2 == 0 and rise**2 == 1
-    if moved_vertically_once: 
+    if chess_tools.moved_pawn(from_position, to_position): 
         return to_position
     
 def rook_to(from_position: tuple, to_position: tuple) -> tuple:
-    run, rise = distance_between(from_position, to_position)
-    moved_horizontally = rise**2 == 0
-    moved_vertically = run**2 == 0
-    if moved_horizontally ^ moved_vertically:
+    if chess_tools.moved_rook(from_position, to_position):
         return to_position 
         
 def knight_to(from_position: tuple, to_position: tuple) -> tuple:
-    run, rise = distance_between(from_position, to_position)
-    if run**2 + rise**2 == 5:
+    if chess_tools.moved_knight(from_position, to_position):
         return to_position
 
 def bishop_to(from_position: tuple, to_position: tuple) -> tuple:
-    run, rise = distance_between(from_position, to_position)
-    moved_diagonally = run**2 == rise**2
-    if moved_diagonally:
+    if chess_tools.moved_bishop(from_position, to_position):
         return to_position
 
 def king_to(from_position: tuple, to_position: tuple) -> tuple:
-    run, rise = distance_between(from_position, to_position)
-    moved_horizontally_once = run**2 == 1 and rise**2 == 0
-    moved_vertically_once = run**2 == 0 and rise**2 == 1
-    moved_diagonally_once = run**2 == 1 and rise**2 == 1
-    if moved_horizontally_once ^ moved_vertically_once:
-        return to_position
-    elif moved_diagonally_once:
+    if chess_tools.moved_king(from_position, to_position):
         return to_position
 
 def queen_to(from_position: tuple, to_position: tuple) -> tuple:
-    run, rise = distance_between(from_position, to_position)
-    moved_horizontally = rise**2 == 0
-    moved_vertically = run**2 == 0
-    moved_diagonally = run**2 == rise**2
-    if moved_diagonally:
+    if chess_tools.moved_queen(from_position, to_position):
         return to_position
-    elif moved_horizontally ^ moved_vertically:
-        return to_position
-
 
 if __name__ == '__main__':
-    x = queen_to((2,1),(4,1))
-    print(x)       
+    x = chess_tools.moved_rook((2,1),(4,1))
+    y = chess_tools.moved_bishop((2,1),(4,1))
+    print(x)      
+    print(y) 
+    print(queen_to((2,1),(4,1)))
